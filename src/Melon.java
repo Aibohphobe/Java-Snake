5a86aa805229de32f2810e7b066868f34332cfe7
Aiboh <121984323+Aibohphobe@users.noreply.github.com>
2023-01-04 18:51:45 -0500

    Add files via upload

---
+++ src/Melon.java
@@ -0,0 +1,8 @@
+public class Melon extends Food 
+{
+    public Melon()
+    {
+        super(); //use the X coord / Y coord generation from Food
+        this.value = 3; //set value of Melon to 3
+    }
+}
---
+++ src/Orange.java
@@ -0,0 +1,8 @@
+public class Orange extends Food
+{   
+    public Orange()
+    {
+        super(); //use the X coord / Y coord generation from Food
+        this.value = 2; //set value of Orange to 2
+    }
+}
